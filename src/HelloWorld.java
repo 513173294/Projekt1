ffac640ee89539422bbc0b4f26d0fc731ee81bcc
Mateusz Barzantny <mateusz.barzantny@op.pl>
2018-05-15 19:04:39 +0200

    dodanie kilku linijek

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("abc");
         System.out.println("abc2");
+        System.out.println("abc2");
+        System.out.println("abc2");
+        System.out.println("abc2");
 
 
     }
